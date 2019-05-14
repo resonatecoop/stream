e24d7c34e1c3d4fb71cb058f0c111d724db5593c
Augustin Godiscal <dev@auggod.io>
2019-05-14 10:14:55 +0200

    Add all country codes for eu

--- beta/lib/country-codes.js
+++ beta/lib/country-codes.js
@@ -1,8 +1,3 @@
 // List of ISO 3166-1 alpha-2 country code
-const vatEu = [
-  "FR",
-  "GB"
-  // TO BE COMPLETED
-]
 
-module.exports = vatEu
+module.exports = ['AX', 'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FO', 'FI', 'FR', 'GF', 'DE', 'GI', 'GR', 'HU', 'IE', 'IM', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'GB']
