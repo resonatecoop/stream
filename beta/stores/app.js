128d08891a54c523e97413979a32174d05b73975
Augustin Godiscal <dev@auggod.io>
2019-09-03 10:04:37 +0200

    Immediatly logout user if auth and play count requests fails with 401

--- beta/stores/app.js
+++ beta/stores/app.js
@@ -211,28 +211,27 @@ function app () {
           if (response.status !== 401) {
             const { accessToken: token, clientId } = response
             state.api = generateApi({ token, clientId, user: state.api.user })
-
-            emitter.emit('api:ok')
-
-            emitter.emit(state.events.RENDER)
+          } else {
+            emitter.emit('logout')
           }
         } else {
           state.api = generateApi()
-
-          emitter.emit('api:ok')
-
-          emitter.emit(state.events.RENDER)
         }
       } catch (err) {
         log.error(err)
+      } finally {
+        emitter.emit('api:ok')
+        emitter.emit(state.events.RENDER)
       }
     })
 
-    emitter.on('logout', () => {
+    emitter.on('logout', (redirect = false) => {
       state.user = {}
       state.api = generateApi()
       storage.clear() // clear everything in indexed db
-      emitter.emit(state.events.PUSHSTATE, '/login')
+      if (redirect) {
+        emitter.emit(state.events.PUSHSTATE, '/login')
+      }
     })
 
     emitter.on(state.events.DOMCONTENTLOADED, () => {
