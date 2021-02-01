ab4962de084d52866d7afa64b9a6f525394564a6
Augustin Godiscal <hello@auggod.tech>
2021-02-01 14:31:13 +0100

    Cleanup

--- beta/lib/update-counter.js
+++ beta/lib/update-counter.js
@@ -1,26 +1,16 @@
 const PlayCount = require('@resonate/play-count')
 const renderCounter = require('@resonate/counter')
 
-function updateCounter (props, element) {
-  const { count, id } = props
+module.exports = props => {
+  const { id, count } = props
 
-  const playCount = new PlayCount(count)
-
-  const counter = renderCounter(`cid-${id}`)
-
-  playCount.counter = counter
-
-  if (element) {
-    const parent = element.parentNode
-    parent.replaceChild(playCount.counter, element)
-  }
-}
-
-module.exports = setPlaycount
-
-function setPlaycount (props) {
-  const { id } = props
-  for (const counter of [...document.querySelectorAll(`#cid-${id}`)]) {
-    updateCounter(props, counter)
+  for (const element of [...document.querySelectorAll(`svg[data-cid="cid-${id}"]`)]) {
+    if (element) {
+      const playCount = new PlayCount(count)
+      const counter = renderCounter(`cid-${id}`)
+      playCount.counter = counter
+      const parent = element.parentNode
+      parent.replaceChild(playCount.counter, element)
+    }
   }
 }
