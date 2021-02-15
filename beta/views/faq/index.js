966104ab5f61739b6c126d682736a023d54d329a
Augustin Godiscal <hello@auggod.tech>
2021-02-15 21:04:29 +0100

    Update FAQ

--- beta/views/faq/index.js
+++ beta/views/faq/index.js
@@ -4,44 +4,76 @@ const raw = require('nanohtml/raw')
 
 const items = [
   {
-    question: 'Uploading music',
-    answer: "We are currently developing our new Artist Upload Tool. Until development and testing are finished, <a href='https://resonate.is/music/upload/' target='_blank' rel='noopener noreferer'>our guide on how to upload to Resonate</a> is the place to go."
+    question: 'I have a Resonate Artist Account. How do I upload music?',
+    answer: 'Once it is ready, our new Upload Tool will put the upload process under your control. For now, volunteers at Resonate will upload your music for you. To submit music, please log in to your Artist Account at <a href="https://resonate.is/" target="_blank">https://resonate.is</a>, complete your Artist Profile, and then follow the guidance on the Uploading Music page.'
   },
   {
-    question: 'High res downloads',
-    answer: "We're working hard to make this possible in the future. Please stay with us!"
+    question: 'How do I get a Resonate Artist Account?',
+    answer: 'Soon, we will be re-enabling direct sign-up for new Artist Accounts. Until then, please email us at <a href="mailto:members@resonate.is">members@resonate.is</a> to join our artist waitlist. We will reach out to you soon to create your artist account and upload your music with help from our volunteers.'
   },
   {
-    question: 'High res audio streaming',
-    answer: "We're planning on enabling 128 bits AAC for everyone and 256 bits AAC will be available on demand."
+    question: 'How do I get a Resonate Label Account?',
+    answer: 'While we build our new catalog processing infrastructure, we have paused the creation of new Label Accounts. <a href="https://resonate.is/join/artist/just-one-more-step/" target="_blank">Join label waitlist</a>'
   },
   {
-    question: 'When will I own a track?',
-    answer: 'You own a track only after you have played it 9 times. Once, you own a track you can listen to it indefinitely. If you want to skip this process and reward the creator now, you may also buy the remaining plays at any time.'
+    question: 'Can I download music I own on Resonate?',
+    answer: 'In the future, we intend to offer the ability to download tracks that you own on Resonate to your local device. This feature is not yet available.'
   },
   {
-    question: 'How much does listening will cost me?',
-    answer: "It really depends how you use the player. If you usually play songs one to three time, your credits may last much longer. <a href='https://resonate.is/stream2own/' target='_blank' rel='noopener noreferer'>Learn more about our stream2own model</a>."
+    question: 'What is Resonate\'s streaming quality?',
+    answer: 'Our current streaming quality is 96 kbps AAC. In the future we intend to offer 128 bits AAC for all users. 256 bits AAC will be available on demand.'
+  },
+  {
+    question: 'How do I add Credits to my account?',
+    answer: 'Your new Resonate account comes with a small amount of Credits, on the house! As you listen to music on Resonate, Credits are deducted automatically from your account. You can purchase more credits for your account by opening the three-dot menu in your Main Menu. Resonate only deducts from the credits you have previously purchased for your account; there are no fees or other charges.'
+  },
+  {
+    question: 'How much does a single Credit cost?',
+    answer: '1 Credit / ~1.23 EUR / ~1.49 USD'
+  },
+  {
+    question: 'How much does it cost to purchase a song?',
+    answer: 'In total, a little more than 1 Credit. 1.022 Credits / ~1.25 EUR / ~1.52 USD'
+  },
+  {
+    question: 'How do I use Credits on Resonate?',
+    answer: 'The Resonate Player keeps track of how many times you\'ve listened to each song. As you listen, Credits are deducted automatically from your account using our unique Stream2own system, described below. You can also spend Credits by using the Buy Now button within a track\'s three-dot menu.'
+  },
+  {
+    question: 'How does Stream2own work? How many Credits per play?',
+    answer: `
+      <p>Stream2own lets you affordably explore new music while rewarding the artists you love -- without being interruped by transactions, advertisements, and paywalls. For each song, the price per play starts tiny and increases bit by bit, reflecting the unique relationship between each listener and the artists they listen to.
+      </p>
+      <ul>
+        <li>Your purchase of a particular song is split over the first nine times you play it.</li>
+        <li>The first time you play that song it is 0.002 Credits / 0.0024 EUR / 0.0029 USD.</li>
+        <li>Each time you return to that song and play it, the price doubles.</li>
+        <li>The ninth play is 0.512 Credits / 0.625 EUR / 0.76 USD.</li>
+      </ul>
+      <p>
+        After the ninth play of that song, you own it! You can stream that song for free on Resonate as much as you want. All songs you own will appear under the Collection tab inside your Library.
+      </p>
+    `
   },
   {
     question: 'How to change my password',
-    answer: "<a href='https://resonate.is/account-settings/password/' target='_blank' rel='noopener noreferer'>Change your password</a>."
-  },
-  {
-    question: 'I can\'t login to the player',
-    answer: 'Before, requesting our support, please check that you are indeed using the email associated to your Resonate account and not something else. If you have just asked for a new password, you may have just to wait a few seconds to be able to use your new password. Also, we are aware that your password may not be accepted if it contains certain non alphanumerics. If this is the case, please request a new password.'
+    answer: '<a href="https://resonate.is/account-settings/password/" target="_blank">Change your password</a>.'
   },
   {
     question: 'I forgot my password',
-    answer: "If you can't remember your password, you should <a href='https://resonate.is/password-reset'>request a new password from our website</a>. It might take up to a minute before you can use your new passwod to login on the beta app or upload tool."
+    answer: 'If you can\'t remember your password, you can <a href="https://resonate.is/password-reset" target="_blank">request a new password from our website</a>.'
+  },
+  {
+    question: 'I can\'t login to the player',
+    answer: 'Before requesting support, make sure that you are using the email associated with your Resonate account. If you are having issues, please request a new password. You may have to wait up to a minute for your new password to become active. Your password might not be accepted if it contains certain non-alphanumerics such as " and \\. If you have tried requesting a new password and are still having difficulties, please contact us at <a href="mailto:members@resonate.is">members@resonate.is</a> and describe your issue in detail.'
   },
   {
     question: 'How to delete my account completely ?',
-    answer: "You can <a href='https://resonate.is/account-settings/delete/' target='_blank' rel='noopener noreferer'>delete your account</a> and all your data at anytime."
+    answer: 'You can <a href="https://resonate.is/account-settings/delete/" target="_blank">delete your account</a> and all your data at anytime.'
   },
   {
     question: 'How to download a copy of all my data ?',
-    answer: "<a href='https://resonate.is/account-settings/privacy/' target='_blank' rel='noopener noreferer'>Download or erase your data</a>."
+    answer: '<a href="https://resonate.is/account-settings/privacy/" target="_blank">Download or erase your data</a>.'
   }
 ]
 
