e03f198190af7959701d32cb6c492136e246ed3a
Augustin Godiscal <hello@auggod.tech>
2021-02-23 21:01:46 +0100

    Adjust breadcrumb position in mobile

--- beta/layouts/browse/index.js
+++ beta/layouts/browse/index.js
@@ -12,9 +12,9 @@ module.exports = (view) => {
   return (state, emit) => {
     return html`
       <main class="flex flex-column flex-auto pb6">
-        <div class="flex flex-row flex-auto w-100">
-          <div class="flex flex-column">
-            <div class="sticky top-0 top-3-l z-999 bg-near-black bg-transparent-l">
+        <div class="flex flex-column flex-row-l flex-auto w-100">
+          <div class="flex flex-column sticky top-0 top-3-l z-999">
+            <div class="sticky top-0 z-999 top-3-l z-999 bg-near-black bg-transparent-l">
               <button class="${bg} br1 bn w2 h2 ma2 ma3-l" onclick=${() => emit('navigate:back')}>
                 <div class="flex items-center justify-center">
                   ${icon('arrow', { size: 'sm' })}
@@ -22,16 +22,18 @@ module.exports = (view) => {
               </button>
             </div>
           </div>
-          <nav role="navigation" aria-label="Browse navigation" class="dn db-l">
-            <ul class="sticky list menu ma0 pa0 flex flex-column justify-around sticky z-999" style="top:3rem">
-              ${items.map(({ text, href }) => html`
-                <li class="relative flex justify-center w-100${state.href === href ? ' active' : ''}">
-                  <a class="link b near-black near-black--light gray--dark db dim pv2 ph4 w-100" href=${href}>${text}</a>
-                </li>
-              `)}
-            </ul>
-          </nav>
-          ${view(state, emit)}
+          <div class="flex flex-row flex-auto w-100">
+            <nav role="navigation" aria-label="Browse navigation" class="dn db-l">
+              <ul class="sticky list menu ma0 pa0 flex flex-column justify-around sticky z-999" style="top:3rem">
+                ${items.map(({ text, href }) => html`
+                  <li class="relative flex justify-center w-100${state.href === href ? ' active' : ''}">
+                    <a class="link b near-black near-black--light gray--dark db dim pv2 ph4 w-100" href=${href}>${text}</a>
+                  </li>
+                `)}
+              </ul>
+            </nav>
+            ${view(state, emit)}
+          </div>
         </div>
       </main>
     `
