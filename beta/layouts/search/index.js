3f65025d73668cd9852f27692af546c1b7bbc037
Augustin Godiscal <hello@auggod.tech>
2021-02-23 22:45:15 +0100

    *

--- beta/layouts/search/index.js
+++ beta/layouts/search/index.js
@@ -52,9 +52,9 @@ function LayoutSearch (view) {
 
     return html`
       <main class="flex flex-column flex-auto pb6">
-        <div class="flex flex-row flex-auto w-100">
-          <div class="flex flex-column">
-            <div class="sticky top-0 top-3-l z-999 bg-near-black bg-transparent-l">
+        <div class="flex flex-column flex-row-l flex-auto w-100">
+          <div class="flex flex-column sticky top-0 top-3-l z-999">
+            <div class="sticky top-0 z-999 top-3-l z-999 bg-near-black bg-transparent-l">
               <button class="${bg} br1 bn w2 h2 ma2 ma3-l" onclick=${() => emit('navigate:back')}>
                 <div class="flex items-center justify-center">
                   ${icon('arrow', { size: 'sm' })}
