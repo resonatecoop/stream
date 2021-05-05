02fa1b4eab6ce9c7c8e4e352eaf40b2acba48071
Augustin Godiscal <hello@auggod.tech>
2021-05-05 13:55:01 +0200

    more defensive

--- beta/lib/resolve-plays-favorites.js
+++ beta/lib/resolve-plays-favorites.js
@@ -10,20 +10,27 @@ function resolvePlaysAndFavorites (ids) {
    */
   return async (state) => {
     // TODO replace with v2 swagger api gen
+    let counts = {}
+    let favorites = {}
+
     const { res1, res2 } = await hash({
       res1: state.apiv2.plays.resolve({ ids }),
       res2: state.apiv2.favorites.resolve({ ids })
     })
 
-    const counts = res1.data.reduce((o, item) => {
-      o[item.track_id] = item.count
-      return o
-    }, {})
+    if (res1.data) {
+      counts = res1.data.reduce((o, item) => {
+        o[item.track_id] = item.count
+        return o
+      }, {})
+    }
 
-    const favorites = res2.data.reduce((o, item) => {
-      o[item.track_id] = item.track_id
-      return o
-    }, {})
+    if (res2.data) {
+      favorites = res2.data.reduce((o, item) => {
+        o[item.track_id] = item.track_id
+        return o
+      }, {})
+    }
 
     return [counts, favorites]
   }
