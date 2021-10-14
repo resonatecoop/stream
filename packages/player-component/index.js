58328ac535e0ed7df36e9a2e41f88f8c35f770b9
Augustin Godiscal <hello@auggod.tech>
2021-10-14 23:06:22 +0200

    Set APP_HOST for menu

--- packages/player-component/index.js
+++ packages/player-component/index.js
@@ -528,7 +528,7 @@ class Player extends Nanocomponent {
       data: Object.assign({}, this.local.track, {
         count: this.local.count,
         favorite: this.local.favorite || this.local.fav,
-        url: new URL(`/track/${this.local.track.id}`, 'https://beta.stream.resonate.coop')
+        url: new URL(`/track/${this.local.track.id}`, process.env.APP_HOST || 'https://stream.resonate.coop')
       }),
       size: this.local.type === 'album' ? 'sm' : 'md', // button size
       orientation: 'topright'
