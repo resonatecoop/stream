8f2c79e1275684e1f2981632e846045fd6c8bba0
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2022-03-03 22:44:19 +0100

    Make initialization synchronous

--- beta/src/index.ts
+++ beta/src/index.ts
@@ -3,22 +3,18 @@ import 'cross-fetch/polyfill'
 import { isBrowser } from 'browser-or-node'
 import Choo from 'choo'
 import choometa from 'choo-meta'
-import stores from './stores/index.js'
+import stores from './stores/index'
+import { browser } from './browser'
 import { routes } from './routes'
 
-async function initialize (): Promise<void> {
-  const app = new Choo()
-  app.use(choometa())
+const app = new Choo()
+app.use(choometa())
 
-  if (isBrowser) {
-    const { browser } = await import('./browser')
-    await browser(app)
-  }
-
-  stores(app)
-  routes(app)
-
-  app.mount('#app')
+if (isBrowser) {
+  browser(app)
 }
 
-void initialize()
+stores(app)
+routes(app)
+
+export default app.mount('#app')
