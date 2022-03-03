7771a62a580426618e3abb5929f1ae190ef5dfa2
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2022-03-03 22:56:49 +0100

    Add comment

--- beta/src/index.ts
+++ beta/src/index.ts
@@ -17,4 +17,7 @@ if (isBrowser) {
 stores(app)
 routes(app)
 
+// This needs to be exported in this common-js-compatible manner since the Choo server won't be able to import it
+// properly otherwise (which will break SSR). Please also note that the Choo return type for `mount` is incorrect.
+// It is not `void` when ran on a server.
 export = app.mount('#app')
