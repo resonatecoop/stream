031b253db9fbca9204fdf33b9feb474060b38375
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2022-03-22 23:36:07 +0100

    Improve tracks types

--- packages/api-service/tracks.ts
+++ packages/api-service/tracks.ts
@@ -1,20 +1,36 @@
-import { APIResponse, ClientGetter } from './types'
+import { APIResponse, ClientGetter, Cover, CoverMetadata, PagedRequest, PagedResponse } from './types'
 
 export interface Track {
   id: number
-  album: object
-  artist: object
-  cover: string
+  creator_id: number
   title: string
+  duration: number
+  album: string | null
+  year?: number | null
+  artist: string
+  cover: string
+  cover_metadata: CoverMetadata
+  status: string
   url: string
+  images: {
+    small: Cover
+    medium: Cover
+    large: Cover
+  }
 }
 
 export interface TrackResponse {
   data: Track
 }
 
+export interface TracksResponse extends PagedResponse<Track> {}
+
+export interface LatestTracksResponse extends Omit<PagedResponse<Track>, 'status'> {}
+
 interface TracksClient {
   getTrack: (params: { id: number }) => Promise<APIResponse<TrackResponse>>
+  getTracks: (params: PagedRequest & { order?: string }) => Promise<APIResponse<TracksResponse>>
+  getLatestTracks: (params: PagedRequest & { order?: string }) => Promise<APIResponse<LatestTracksResponse>>
 }
 
 export async function getTracksClient (clientGetter: ClientGetter): Promise<TracksClient> {
--- packages/api-service/types.ts
+++ packages/api-service/types.ts
@@ -4,12 +4,24 @@ export interface APIResponse<T> {
   body: T
 }
 
+export interface PagedRequest {
+  limit?: number
+  page?: number
+}
+
+export interface PagedResponse<T> {
+  data: T[]
+  count: number
+  numberOfPages: number
+  status: string
+}
+
 export interface CoverMetadata {
-  id: string
+  id: string | null
 }
 
 export interface Cover {
   height: number
   width: number
-  url: string
+  url?: string
 }
