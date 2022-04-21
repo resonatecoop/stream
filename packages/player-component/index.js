854903e6e2161b8cdb9fd719320359eccf24e22b
Peter Klingelhofer <peterklingelhofer@gmail.com>
2022-04-21 10:04:40 -0500

    Notify user when credits are too low for full length song playback (#213)
    * notify user when credits low
    
    * remove nested ternary to improve readability
    
    * warn if not enough credits to play current track in full

--- packages/player-component/index.js
+++ packages/player-component/index.js
@@ -316,12 +316,17 @@ class Player extends Nanocomponent {
 
   renderPlayer () {
     const isAuthenticated = !!this.state.user.uid
+    const classesInfoBar = 'flex flex-row w-100 justify-center bb b--light-silver no-underline'
+    let infoBar = ''
+    if (!isAuthenticated) {
+      infoBar = html`<a class=${classesInfoBar} href="/login" target="_blank" rel="noopener noreferer">Log in to listen to full song</a>`
+    } else if (Number(this.state.user.credits) < 0.002 && this.local.count < 9) {
+      infoBar = html`<a class=${classesInfoBar}>You don’t have enough credits to play the current track in full’</a>`
+    }
 
     return html`
       <div class="player-component flex flex-column h-100">
-        ${!isAuthenticated
-          ? html`<a class="flex flex-row w-100 justify-center bb b--light-silver no-underline" href="/login" target="_blank" rel="noopener noreferer">Log in to listen to full song</a>`
-          : ''}
+        ${infoBar}
         ${this.renderArtwork()}
         ${this.renderControls()}
       </div>
