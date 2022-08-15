e94bb09a30bbb8b46ac7ff5fd4d550fcc4c5920f
Peter Klingelhofer <peterklingelhofer@gmail.com>
2022-08-15 05:28:41 -0500

    fix: api v3 login links

--- beta/src/components/header/index.ts
+++ beta/src/components/header/index.ts
@@ -177,11 +177,9 @@ class Header extends Component<HeaderProps> {
               `
               : html`<li class="flex flex-auto w-100 justify-center" role="divider"></li>`}
             <li class="${this.state.resolved && !this.state.user.uid ? 'flex' : 'dn'} flex-auto justify-center w-100 grow" role="menuitem">
-            ${this.state.href !== '/login'
-              ? html`<a class="link pv1 ph3 ttu ba b--mid-gray b--dark-gray--dark db f6 b white-space-no-wrap" href=${AUTH_HREF} >
-                  Log In
-                </a>`
-              : ''}
+              <a class="link pv1 ph3 ttu ba b--mid-gray b--dark-gray--dark db f6 b white-space-no-wrap" href=${AUTH_HREF} >
+                Log In
+              </a>
             </li>
             <li class="${this.state.resolved ? 'dn' : 'flex'} flex-auto w-100 justify-center" role="divider"></li>
             <li class="${!this.state.user.uid ? 'dn' : 'flex'} flex-auto justify-center w-100" role="menuitem">
